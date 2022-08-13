641fd457f9132161539b4d54f2fd010747624107
Juan Carlos Larrondo <juancarlos1221@gmail.com>
2022-08-13 15:07:06 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import http from 'http'
+import https from 'http'
 import doctors from './src/routers/doctors.router'
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 5000;
+const environment =  process.env.ENVIRONMENT || 'DEVELOP';
 
 app.use('/doctors', doctors);
 
-http.createServer(app).listen(port);
-console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+if(environment === 'PRODUCTION') {
+  https.createServer(app).listen(port);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+} else {
+  http.createServer(app).listen(port);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+}
+
+
